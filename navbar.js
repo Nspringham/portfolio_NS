23252d26c49b2f0cbe34d0c1541c7a3291df4c0e
Nick Springham <Nspringham@aol.co.uk>
2018-06-27 21:15:54 +0100

    Initial files

---
+++ navbar.js
@@ -0,0 +1,7 @@
+function classToggle() {
+    const navs = document.querySelectorAll('.Navbar__Items')
+    
+    navs.forEach(nav => nav.classList.toggle('.menu-item-toggle-show'));
+  }
+  
+  document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
