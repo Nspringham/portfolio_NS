89eb911146dd7338782ea7647dc6f91241eb3d3f
Nick Springham <nickspringham7@gmail.com>
2018-08-01 18:56:28 +0100

    tidied up snippet

--- navbar.js
+++ navbar.js
@@ -1,7 +1,5 @@
 function classToggle() {
-  const navs = document.querySelectorAll('.menu-items')
-    
+  const navs = document.querySelectorAll('.menu-items') 
   navs.forEach(nav => nav.classList.toggle('menu-item-toggle-show'));
 }
-  
 document.querySelector('.Navbar__Link-toggle').addEventListener('click', classToggle);
